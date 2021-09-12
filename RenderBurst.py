b1cb98cdd40b5d01aadb3697cb2d2bcc662c5334
TheKenetics <TheKenetics@users.noreply.github.com>
2021-09-12 15:01:54 -0500

    Refactoring and UI changes

--- RenderBurst.py
+++ RenderBurst.py
@@ -8,37 +8,12 @@ bl_info = {
 			"Render all cameras, one by one, and store results.",
 }
 
-import bpy
-import os
+import bpy, os
 from bpy.props import EnumProperty, IntProperty, IntVectorProperty, FloatVectorProperty, BoolProperty, FloatProperty, StringProperty
 from bpy.types import PropertyGroup, UIList, Operator, Panel, AddonPreferences
 
-
-def SetRenderBorderMinX(self, new_value):
-	self["render_border_min_x"] = Clamp(new_value, 0.0, self.render_border_max_x)
-
-def SetRenderBorderMaxX(self, new_value):
-	self["render_border_max_x"] = Clamp(new_value, self.render_border_min_x, 1.0)
-
-def SetRenderBorderMinY(self, new_value):
-	self["render_border_min_y"] = Clamp(new_value, 0.0, self.render_border_max_y)
-
-def SetRenderBorderMaxY(self, new_value):
-	self["render_border_max_y"] = Clamp(new_value, self.render_border_min_y, 1.0)
-
-class RbCameraRenderSettings(PropertyGroup):
-	use_camera_settings : BoolProperty(name="Use Camera Settings", default=True)
-	render_size : IntVectorProperty(name="Render Size", size=2, default=(1920,1080), min=0)
-	render_percentage : FloatProperty(name="Render Percentage", default=100.0, min=0.0)
-	use_render_border : BoolProperty(name="Use Render Border", default=False)
-	crop_to_render_border : BoolProperty(name="Crop to Render Border", default=False)
-	render_border_min_x : FloatProperty(name="Render Border Min X", default=0.0, min=0.0, max=1.0)#, set=SetRenderBorderMinX)
-	render_border_max_x : FloatProperty(name="Render Border Max X", default=1.0, min=0.0, max=1.0)#, set=SetRenderBorderMaxX)
-	render_border_min_y : FloatProperty(name="Render Border Min Y", default=0.0, min=0.0, max=1.0)#, set=SetRenderBorderMinY)
-	render_border_max_y : FloatProperty(name="Render Border Max Y", default=1.0, min=0.0, max=1.0)#, set=SetRenderBorderMaxY)
-
-
-# Set current render settings from camera settings
+## Helper Functions
+# Set camera settings from current render settings
 def SetCameraSettingsFromRenderSettings(context, camera_render_settings):
 	render = context.scene.render
 	camera_render_settings.render_size[0] = render.resolution_x
@@ -51,7 +26,7 @@ def SetCameraSettingsFromRenderSettings(context, camera_render_settings):
 	camera_render_settings.render_border_min_y = render.border_min_y
 	camera_render_settings.render_border_max_y = render.border_max_y
 
-# Set camera settings from current render settings
+# Set current render settings from camera settings
 def SetRenderSettingsFromCameraSettings(context, camera_render_settings):
 	render = context.scene.render
 	render.resolution_x = camera_render_settings.render_size[0]
@@ -64,114 +39,153 @@ def SetRenderSettingsFromCameraSettings(context, camera_render_settings):
 	render.border_min_y = camera_render_settings.render_border_min_y
 	render.border_max_y = camera_render_settings.render_border_max_y
 
-
-def Clamp(value, min_value, max_value):
-	print(max(min_value, min(max_value, value)))
-	return max(min_value, min(max_value, value))
-
 def ShowMessageBox(message = "", title = "Message Box", icon = 'INFO'):
-
 	def draw(self, context):
 		self.layout.label(text=message)
 
 	bpy.context.window_manager.popup_menu(draw, title = title, icon = icon)
 
 
-class RenderBurst(bpy.types.Operator):
+## Structs
+class RbCameraRenderSettings(PropertyGroup):
+	#use_camera_settings : BoolProperty(name="Use Camera Settings", default=True)
+	render_size : IntVectorProperty(name="Render Size", size=2, default=(1920,1080), min=0)
+	render_percentage : FloatProperty(name="Render Percentage", default=100.0, min=0.0, subtype="PERCENTAGE", soft_max=100)
+	use_render_border : BoolProperty(name="Use Render Border", default=False)
+	crop_to_render_border : BoolProperty(name="Crop to Render Border", default=False)
+	render_border_min_x : FloatProperty(name="Render Border Min X", default=0.0, min=0.0, max=1.0)
+	render_border_max_x : FloatProperty(name="Render Border Max X", default=1.0, min=0.0, max=1.0)
+	render_border_min_y : FloatProperty(name="Render Border Min Y", default=0.0, min=0.0, max=1.0)
+	render_border_max_y : FloatProperty(name="Render Border Max Y", default=1.0, min=0.0, max=1.0)
+	#render_samples : IntProperty(name="Render Samples", default=200, min=0)
+
+
+class RbFilterSettings(PropertyGroup):
+	rb_filter_enum : EnumProperty(
+		name = "Filter",
+		description = "Choose your destiny",
+		items = [
+			("all", "All Cameras", "Render all cameras"),
+			("selected", "Selected Only", "Render selected only"),
+		],
+		default = 'all'
+	)
+
+
+## Operators
+class RenderBurst(Operator):
 	"""Render all cameras"""
 	bl_idname = "render.renderburst"
 	bl_label = "Render Burst"
 
 	_timer = None
 	shots = None
-	stop = None
-	rendering = None
+	is_cancelled = None
+	is_rendering = None
 	path = "//"
-	disablerbbutton = False
 	do_change_file_output_names = False
 	orig_file_output_paths = []
 	file_output_node = None
 
+	def restore_file_output_node_paths(self):
+		for i, slot in enumerate(self.file_output_node.file_slots):
+			slot.path = self.orig_file_output_paths[i]
+
 	def pre(self, dummy, thrd = None):
-		self.rendering = True
+		## Pre-render stuff
+		self.is_rendering = True
 
 	def post(self, dummy, thrd = None):
+		## Post-render stuff
 		self.shots.pop(0) 
-		self.rendering = False
+		self.is_rendering = False
 
 	def cancelled(self, dummy, thrd = None):
-		self.stop = True
+		## Handle render being cancelled
+		self.is_cancelled = True
+		
 		if self.do_change_file_output_names:
-			for i, slot in enumerate(self.file_output_node.file_slots):
-				slot.path = self.orig_file_output_paths[i]
+			self.restore_file_output_node_paths()
 
 	def execute(self, context):
-		self.stop = False
-		self.rendering = False
-		scene = bpy.context.scene
-		wm = bpy.context.window_manager
-		if wm.rb_filter.rb_filter_enum == 'selected':
-			self.shots = [ o.name+'' for o in bpy.context.selected_objects if o.type=='CAMERA' and o.visible_get() == True]
-		else:
-			self.shots = [ o.name+'' for o in bpy.context.visible_objects if o.type=='CAMERA' and o.visible_get() == True ]
-
-
-		if len(self.shots) < 0:
-			self.report({"WARNING"}, 'No cameras defined')
+		## Check if RenderBurst can run
+		if context.scene.render.filepath is None or len(context.scene.render.filepath)<1:
+			self.report({"ERROR"}, 'Output path not defined. Please, define the output path on the render settings panel')
 			return {"FINISHED"}
 
+		animation_formats = [ 'FFMPEG', 'AVI_JPEG', 'AVI_RAW', 'FRAMESERVER' ]
+
+		if context.scene.render.image_settings.file_format in animation_formats:
+			self.report({"ERROR"}, 'Animation formats are not supported. Yet :)')
+			return {"FINISHED"}
+		
+		## Start of operator
+		self.is_cancelled = False
+		self.is_rendering = False
+		scene = context.scene
+		wm = context.window_manager
+		if wm.rb_filter.rb_filter_enum == 'selected':
+			self.shots = [ o.name+'' for o in context.selected_objects if o.type=='CAMERA' and o.visible_get() == True]
+		else:
+			self.shots = [ o.name+'' for o in context.visible_objects if o.type=='CAMERA' and o.visible_get() == True ]
+
+		if not self.shots:
+			self.report({"WARNING"}, 'No cameras defined')
+			return {"FINISHED"}
+		
+		# Register callbacks
 		bpy.app.handlers.render_pre.append(self.pre)
 		bpy.app.handlers.render_post.append(self.post)
 		bpy.app.handlers.render_cancel.append(self.cancelled)
-
-		self._timer = bpy.context.window_manager.event_timer_add(0.5, window=bpy.context.window)
-		bpy.context.window_manager.modal_handler_add(self)
+		
+		self._timer = wm.event_timer_add(0.5, window=bpy.context.window)
+		wm.modal_handler_add(self)
 
 		# check if we should change file output names
 		if context.scene.use_nodes and context.scene.node_tree:
 			# scan for a file output node
 			for node in context.scene.node_tree.nodes:
 				if node.type == "OUTPUT_FILE":
+					# If 1st path name includes the python format placeholder '{}'
 					if "{}" in node.file_slots[0].path:
-						self.file_output_node = node
 						self.do_change_file_output_names = True
+						self.file_output_node = node
 						
 						for slot in node.file_slots:
 							self.orig_file_output_paths.append(slot.path)
-					
-
+						break
+		
 		return {"RUNNING_MODAL"}
 
 	def modal(self, context, event):
 		if event.type == 'TIMER':
-
-			if True in (not self.shots, self.stop is True): 
+			## if rendering gets cancelled or we are out of cameras
+			if self.is_cancelled or not self.shots:
 				bpy.app.handlers.render_pre.remove(self.pre)
 				bpy.app.handlers.render_post.remove(self.post)
 				bpy.app.handlers.render_cancel.remove(self.cancelled)
-				bpy.context.window_manager.event_timer_remove(self._timer)
+				context.window_manager.event_timer_remove(self._timer)
 
 				if self.do_change_file_output_names:
-					for i, slot in enumerate(self.file_output_node.file_slots):
-						slot.path = self.orig_file_output_paths[i]
+					self.restore_file_output_node_paths()
 
 				return {"FINISHED"}
 
-			elif self.rendering is False: 
-				sc = bpy.context.scene
-				sc.camera = bpy.data.objects[self.shots[0]]
+			elif self.is_rendering is False: 
+				scene = context.scene
+				scene.camera = bpy.data.objects[self.shots[0]]
+				cam = scene.camera
 				# Set render settings from camera settings
-				if sc.camera.data.rb_camera_render_settings.use_camera_settings:
-					SetRenderSettingsFromCameraSettings(context, sc.camera.data.rb_camera_render_settings)
+				#if cam.data.rb_camera_render_settings.use_camera_settings:
+				SetRenderSettingsFromCameraSettings(context, cam.data.rb_camera_render_settings)
 
-				# format names
+				# format file output node names
 				if self.do_change_file_output_names:
 					for i, slot in enumerate(self.file_output_node.file_slots):
-						print(self.orig_file_output_paths[i].format(context.scene.camera.name))
 						slot.path = self.orig_file_output_paths[i].format(context.scene.camera.name)
 
 				lpath = self.path
-				fpath = sc.render.filepath
+				fpath = scene.render.filepath
 				is_relative_path = True
 
 				if fpath != "":
@@ -192,25 +206,44 @@ class RenderBurst(bpy.types.Operator):
 						lpath="/" 
 					lpath+="/"
 
-				sc.render.filepath = lpath + self.shots[0] + sc.render.file_extension
+				scene.render.filepath = lpath + self.shots[0] + scene.render.file_extension
 				bpy.ops.render.render("INVOKE_DEFAULT", write_still=True)
 
 		return {"PASS_THROUGH"}
 
-# ui part
-class RbFilterSettings(bpy.types.PropertyGroup):
-	rb_filter_enum : bpy.props.EnumProperty(
-		name = "Filter",
-		description = "Choose your destiny",
-		items = [
-			("all", "All Cameras", "Render all cameras"),
-			("selected", "Selected Only", "Render selected only"),
-		],
-		default = 'all'
-	)
+
+class OBJECT_OT_RBSetCameraSettingsFromRenderSettings(Operator):
+	bl_idname = "rb.set_camera_settings_from_render_settings"
+	bl_label = "Set Camera Settings From Render Settings"
+
+	@classmethod
+	def poll(cls, context):
+		return context.scene.camera
+		#return context.active_object and context.active_object.type == "CAMERA"
+
+	def execute(self, context):
+		SetCameraSettingsFromRenderSettings(context, context.scene.camera.data.rb_camera_render_settings)
+		#SetCameraSettingsFromRenderSettings(context, context.active_object.data.rb_camera_render_settings)
+		return{'FINISHED'}
 
 
-class RenderBurstCamerasPanel(bpy.types.Panel):
+class OBJECT_OT_RBSetRenderSettingsFromCameraSettings(Operator):
+	bl_idname = "rb.set_render_settings_from_camera_settings"
+	bl_label = "Set Render Settings From Camera Settings"
+
+	@classmethod
+	def poll(cls, context):
+		return context.scene.camera
+		#return context.active_object and context.active_object.type == "CAMERA"
+
+	def execute(self, context):
+		SetRenderSettingsFromCameraSettings(context, context.scene.camera.data.rb_camera_render_settings)
+		#SetRenderSettingsFromCameraSettings(context, context.active_object.data.rb_camera_render_settings)
+		return{'FINISHED'}
+
+
+## UI
+class RenderBurstCamerasPanel(Panel):
 	"""Creates a Panel in the scene context of the properties editor"""
 	bl_label = "Render Burst"
 	bl_idname = "SCENE_PT_layout"
@@ -220,59 +253,11 @@ class RenderBurstCamerasPanel(bpy.types.Panel):
 
 	def draw(self, context):
 		wm = context.window_manager
-		row = self.layout.row()
+		#layout = self.layout
+		col = self.layout.column(align=True)
+		row = col.row(align=True)
 		row.prop(wm.rb_filter, "rb_filter_enum", expand=True)
-		row = self.layout.row()
-		row.operator("rb.renderbutton", text='Render!')
-		row = self.layout.row()
-
-class OBJECT_OT_RBButton(bpy.types.Operator):
-	bl_idname = "rb.renderbutton"
-	bl_label = "Render"
-
-	#@classmethod
-	#def poll(cls, context):
-	#    return True
-
-	def execute(self, context):
-		if bpy.context.scene.render.filepath is None or len(bpy.context.scene.render.filepath)<1:
-			self.report({"ERROR"}, 'Output path not defined. Please, define the output path on the render settings panel')
-			return {"FINISHED"}
-
-		animation_formats = [ 'FFMPEG', 'AVI_JPEG', 'AVI_RAW', 'FRAMESERVER' ]
-
-		if bpy.context.scene.render.image_settings.file_format in animation_formats:
-			self.report({"ERROR"}, 'Animation formats are not supported. Yet :)')
-			return {"FINISHED"}
-
-		bpy.ops.render.renderburst()
-		return{'FINISHED'}
-
-
-class OBJECT_OT_RBSetCameraSettingsFromRenderSettings(bpy.types.Operator):
-	bl_idname = "rb.set_camera_settings_from_render_settings"
-	bl_label = "Set Camera Settings From Render Settings"
-
-	@classmethod
-	def poll(cls, context):
-		return context.active_object and context.active_object.type == "CAMERA"
-
-	def execute(self, context):
-		SetCameraSettingsFromRenderSettings(context, context.active_object.data.rb_camera_render_settings)
-		return{'FINISHED'}
-
-
-class OBJECT_OT_RBSetRenderSettingsFromCameraSettings(bpy.types.Operator):
-	bl_idname = "rb.set_render_settings_from_camera_settings"
-	bl_label = "Set Render Settings From Camera Settings"
-
-	@classmethod
-	def poll(cls, context):
-		return context.active_object and context.active_object.type == "CAMERA"
-
-	def execute(self, context):
-		SetRenderSettingsFromCameraSettings(context, context.active_object.data.rb_camera_render_settings)
-		return{'FINISHED'}
+		col.operator(RenderBurst.bl_idname, text='Render!')
 
 
 class Camera_PT_RBCameraRenderSettings(Panel):
@@ -290,51 +275,57 @@ class Camera_PT_RBCameraRenderSettings(Panel):
 	def draw(self, context):
 		camera_render_settings = context.active_object.data.rb_camera_render_settings
 		layout = self.layout
-		layout.prop(camera_render_settings, "use_camera_settings")
-		layout.prop(camera_render_settings, "render_size")
-		layout.prop(camera_render_settings, "render_percentage")
+		layout.use_property_split = True
+		#layout.prop(camera_render_settings, "use_camera_settings")
+		col = layout.column(align=True)
+		col.prop(camera_render_settings, "render_size")
+		col.prop(camera_render_settings, "render_percentage", text="%")
 		layout.prop(camera_render_settings, "use_render_border")
 		layout.prop(camera_render_settings, "crop_to_render_border")
-		layout.prop(camera_render_settings, "render_border_min_x")
-		layout.prop(camera_render_settings, "render_border_max_x")
-		layout.prop(camera_render_settings, "render_border_min_y")
-		layout.prop(camera_render_settings, "render_border_max_y")
-		#layout.operator(OBJECT_OT_RBSetCameraSettingsFromRenderSettings.bl_idname)
+		#layout.prop(camera_render_settings, "render_border_min_x")
+		#layout.prop(camera_render_settings, "render_border_max_x")
+		#layout.prop(camera_render_settings, "render_border_min_y")
+		#layout.prop(camera_render_settings, "render_border_max_y")
+
 
 def menu_func(self, context):
 	self.layout.operator(RenderBurst.bl_idname)
 
 def draw_set_camera_settings(self, context):
-	self.layout.operator(OBJECT_OT_RBSetCameraSettingsFromRenderSettings.bl_idname)
-	self.layout.operator(OBJECT_OT_RBSetRenderSettingsFromCameraSettings.bl_idname)
+	layout = self.layout
+	col = layout.column(align=True)
+	col.label(text="RenderBurst SceneCam Render Settings", icon="RESTRICT_RENDER_OFF")
+	split = col.split(factor=0.5, align=True)
+	split.operator(OBJECT_OT_RBSetRenderSettingsFromCameraSettings.bl_idname, text="Get Settings", icon="WORKSPACE")
+	split.operator(OBJECT_OT_RBSetCameraSettingsFromRenderSettings.bl_idname, text="Set Settings", icon="IMPORT")
+
+## Register
+classes = (
+	RbFilterSettings,
+	RbCameraRenderSettings,
+	Camera_PT_RBCameraRenderSettings,
+	RenderBurst,
+	OBJECT_OT_RBSetCameraSettingsFromRenderSettings,
+	OBJECT_OT_RBSetRenderSettingsFromCameraSettings,
+	RenderBurstCamerasPanel
+)
 
 def register():
-	from bpy.utils import register_class
-	register_class(RbFilterSettings)
-	register_class(RbCameraRenderSettings)
-	register_class(Camera_PT_RBCameraRenderSettings)
+	for cls in classes:
+		bpy.utils.register_class(cls)
+	
 	bpy.types.WindowManager.rb_filter = bpy.props.PointerProperty(type=RbFilterSettings)
 	bpy.types.Camera.rb_camera_render_settings = bpy.props.PointerProperty(type=RbCameraRenderSettings)
-	register_class(RenderBurst)
-	register_class(OBJECT_OT_RBSetCameraSettingsFromRenderSettings)
-	register_class(OBJECT_OT_RBSetRenderSettingsFromCameraSettings)
-	register_class(RenderBurstCamerasPanel)
-	register_class(OBJECT_OT_RBButton)
 	bpy.types.TOPBAR_MT_render.append(menu_func)
 	bpy.types.RENDER_PT_dimensions.append(draw_set_camera_settings)
 
 def unregister():
-	from bpy.utils import unregister_class
 	bpy.types.RENDER_PT_dimensions.remove(draw_set_camera_settings)
-	unregister_class(OBJECT_OT_RBSetRenderSettingsFromCameraSettings)
-	unregister_class(OBJECT_OT_RBSetCameraSettingsFromRenderSettings)
-	unregister_class(RenderBurst)
 	bpy.types.TOPBAR_MT_render.remove(menu_func)
-	unregister_class(RbFilterSettings)
-	unregister_class(RbCameraRenderSettings)
-	unregister_class(Camera_PT_RBCameraRenderSettings)
-	unregister_class(RenderBurstCamerasPanel)
-	unregister_class(OBJECT_OT_RBButton)
+	
+	for cls in reversed(classes):
+		bpy.utils.unregister_class(cls)
+
 	del bpy.types.Camera.rb_camera_render_settings
 
 if __name__ == "__main__":
